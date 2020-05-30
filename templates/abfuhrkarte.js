2a85128811598c5379ea6adaa179fe7db55bc4d0
ubergesundheit <ubergesundheit@googlemail.com>
2020-05-30 17:41:51 +0200

    hide past months

--- templates/abfuhrkarte.js
+++ templates/abfuhrkarte.js
@@ -6,17 +6,30 @@ const makeAttribution = () => {
 };
 
 const hidePastDates = () => {
-  let today = Date.now();
+  const now = new Date();
+  let today = now.getTime();
   today = today - (today % 86400000);
-  for (const dateLink of document.querySelectorAll('li[data-date]')) {
+
+  const thisMonth = new Date(now.toISOString().slice(0, 7)).getTime();
+
+  for (const dateLink of document.querySelectorAll("li[data-date]")) {
     const linkDate = new Date(dateLink.dataset.date).getTime();
     if (today > linkDate) {
-      dateLink.setAttribute('hidden', true);
+      dateLink.setAttribute("hidden", true);
     } else {
       break;
     }
   }
-}
+
+  for (const calendar of document.querySelectorAll(".calendar[data-month]")) {
+    const calendarMonth = new Date(calendar.dataset.month).getTime();
+    if (thisMonth > calendarMonth) {
+      calendar.setAttribute("hidden", true);
+    } else {
+      break;
+    }
+  }
+};
 
 const init = () => {
   // var mymap = L.map("map").setView([51.96, 7.62], 11);
