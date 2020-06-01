e6625e36b476591b74354debb17670b61a6877e6
ubergesundheit <ubergesundheit@googlemail.com>
2020-06-01 16:13:22 +0200

    documentation & automatic dir creation in utils

--- abfuhrkarte/utils.py
+++ abfuhrkarte/utils.py
@@ -1,13 +1,16 @@
-import json
+from json import load, dump
+from os import makedirs
+from pathlib import Path
 
 
 def dict_to_jsonfile(data_dict, path_with_filename):
-    with open(path_with_filename, 'w', encoding='utf-8') as outfile:
-        json.dump(data_dict, outfile,
-                  ensure_ascii=False, sort_keys=True)
+    path = Path(path_with_filename)
+    makedirs(path.parent, exist_ok=True)
+    with open(path, 'w', encoding='utf-8') as outfile:
+        dump(data_dict, outfile,
+             ensure_ascii=False, sort_keys=True)
+
 
 def dict_from_jsonfile(path_with_filename):
     with open(path_with_filename, 'r', encoding='utf-8') as infile:
-        return json.load(infile)
-        # json.dump(data_dict, outfile,
-        #           ensure_ascii=False, sort_keys=True)
+        return load(infile)
